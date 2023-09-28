39126eaad3e3f649a0f769ffc11f66f923b4446a
Amal C P <acp070146@gmail.com>
2023-09-28 17:12:56 +0530

    initil pages defined

---
+++ src/Pages/ProfileArtist/ProfileArtist.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './ProfileArtist.module.css'
+
+const ProfileArtist = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ProfileArtist
