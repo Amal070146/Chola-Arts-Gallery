0601b7d05ace08d593fdd83958727fd237e1ceb8
Amal C P <acp070146@gmail.com>
2023-10-01 02:26:34 +0530

    footer set

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
-import styles from './Footer.module.css'
+import React from "react";
+import styles from "./Footer.module.css";
+import { LogoShortBlack } from "../../assets/svg.tsx";
 const Footer = () => {
   return (
-    <div>
-      
+    <div className={styles.footerWrapper}>
+      <div className={styles.top}>
+        <LogoShortBlack />
+        <div>
+          <a href="">TERMS AND CONDITIONS</a>
+          <a href="">PRIVACY POLICY</a>
+        </div>
+      </div>
+      <div>
+        <p>
+          Copyright © 2023 Chola art Gallery, inc. DESIGNED & DEVELOPED BY{" "}
+          <a href="https://fundesign.vercel.app/" target="_blank">
+            <b> FUN DESIGN</b>
+          </a>
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
